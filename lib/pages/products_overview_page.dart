9c8f8e522c59305a9434882bd5a7f01597669301
MatheusBrun <mte.bruno1@hotmail.com>
2022-07-20 13:21:08 -0700

    configurado provider e adicionado favoritos

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:lojamodel/components/product_item.dart';
-import 'package:lojamodel/data/dummy_data.dart';
-import 'package:provider/provider.dart';
-import '../models/product.dart';
-import '../models/product_list.dart';
+
+import '../components/product_grid.dart';
+
 
 class ProductsOverviewPage extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -18,16 +14,7 @@ class ProductsOverviewPage extends StatelessWidget {
             'Minha Loja',
           ),
         ),
-        body: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-        ));
+        body: ProductGrid());
   }
 }
+
